ee70c0657eb884dae64a6dada05b4562e70926c5
Maria Monteiro <mcmpacheco7@gmail.com>
2024-05-21 19:55:06 -0300

    :sparkles: feat: Adicionando parâmetros Nativos - fs e Http

--- primeira-api/app.js
+++ primeira-api/app.js
@@ -1,16 +1,40 @@
-const http = require('http');
+import http from 'http';
+import fs from 'fs';
+import { error } from 'console';
 
-const servidor = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain;cd charset=utf8');
-    res.end('Olá, Maria!');
+fs.writeFile('./mensagem.txt', 'Olá Maria!','utf-8', (error)=> {
+    if (error) {
+        console.error('Falha ao escrever o Arquivo!', error);
+        return;
+    }
+    console.log('Arquivo criado com Sucesso!');
+});
+
+fs.readFile('./mensagem.txt','utf-8', (error, conteudo) => {
+    if(error) {
+        console.error('Houve uma falha na leitura do Arquivo.', error);
+        return;
+    }
+
+    console.log(`Conteúdo: ${conteudo}`);
+    
+    iniciaServidorHttp(conteudo);
+}); 
+
+function iniciaServidorHttp(mensagem){
+
+    const servidor = http.createServer((req,res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'text/plain; charset=utf8');
+        res.end(mensagem);
+    }
+    );
+
+    const porta = 3000;
+    const host = 'localhost';
+
+    servidor.listen(porta, host, ()=> {
+        console.log(`Servidor executanto em http://${host}:${porta}/`);
+    })
 }
-);
-
-const porta = 3000;
-const host = 'localhost';
-
-servidor.listen(porta, host, ()=> {
-    console.log(`Servidor executanto em http://${host}:${porta}/`);
-})
 
